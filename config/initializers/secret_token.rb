6a4c7792670ebcad523874e1c4ea3e4929322d1b
Wade McEwen <wade@fbsdata.com>
2012-12-08 11:29:22 -0600

    new rails app with boostrap themed ui

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroundControl::Application.config.secret_token = '718d449f9eed567ce6a0b0fc573e598cd805f2036a8b360682cdd8ee61e76d5dadc6a80f3f0298d2184783146299335655456e53c28a3c818c80814ba6130115'
